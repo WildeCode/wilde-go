0c63e875c33afdbb9502dd03b5dfbac258a14c5f
J Alex Long <johnalexanderlong@gmail.com>
2023-02-10 16:37:35 -0600

    Remove CLI class and add Group class
    In addition to leaving a placeholder for the Group class, I changed
    Engine.makeMove to Engine.makeEngineMove to be more clear
    about which method makes a move in the Board or Engine classes.
    Also made a format change where the -> {data type} at the end of
    functions is removed for functions like __init__ or __str__ which are
    clear in what data type they do or don't return.
    
     I also made all variables, methods, etc. convert to snake_case as
     opposed to lowerCamelCase. I do like it better, but since this is an
     open-source project I will try to stand by the PEP8 guidelines.

--- test.py
+++ test.py
@@ -4,8 +4,8 @@ def testGame():
     """tests the Game class by making 10 moves."""
     testGame = wildeStone.Game()
     for i in range(10):
-        move = testGame.engine.selectMove()
-        testGame.board.makeMove(move)
+        move = testGame.engine.select_move()
+        testGame.board.make_move(move)
         print(testGame.engine.board, '\n'*2)
         
 testGame()
